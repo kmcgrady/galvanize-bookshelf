2f7dcac342c5611aea9a6e0d9e22af0a416f0695
rrathbone <rachellerathbone@gmail.com>
2016-10-10 08:48:18 -0700

    complete part 4

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,7 +4,7 @@ const boom = require('boom');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const knex = require('../knex');
-const { camelizeKeys } = require('humps');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -17,10 +17,16 @@ const authorize = function(req, res, next) {
 
     req.token = decoded;
 
-    // You can now access the payload via req.token.userId
     next();
   });
 };
+
+router.get('/favorites/:id', authorize, (req, res, next) => {
+  knex('favorites')
+    .where('book_id', req.query.bookId)
+    .then((favorites) => res.send(favorites.length > 0))
+    .catch((err) => next(err));
+});
 
 router.get('/favorites', authorize, (req, res, next) => {
   const { userId } = req.token;
@@ -28,7 +34,7 @@ router.get('/favorites', authorize, (req, res, next) => {
   knex('favorites')
     .innerJoin('books', 'books.id', 'favorites.book_id')
     .where('favorites.user_id', userId)
-    // .orderBy('books.title', 'ASC')
+    .orderBy('books.title', 'ASC')
     .then((rows) => {
       const favorites = camelizeKeys(rows);
 
@@ -39,37 +45,54 @@ router.get('/favorites', authorize, (req, res, next) => {
     });
 });
 
-router.get('/favorites/check?bookId=1', authorize, (req, res, next) => {
+router.post('/favorites', authorize, (req, res, next) => {
+    const { bookId } = req.body;
+    const favorite = { bookId, userId: req.token.userId };
+
+    if (!bookId)
+      return next(boom.create(400, 'Book id must not be blank'));
+
+    knex('favorites')
+      .insert(decamelizeKeys(favorite), '*')
+      .then((rows) => {
+        favorite.id = rows[0].id;
+        res.send(favorite);
+      })
+      .catch((err) => {
+        next(err);
+      });
+});
+
+router.delete('/favorites', authorize, (req, res, next) => {
+  let favorite;
+
   const { userId } = req.token;
+  const { bookId } = req.body;
+
+  if (typeof bookId !== 'number') {
+    return next(boom.create(400, 'Id must be an integer'));
+  }
 
   knex('favorites')
-    .innerJoin('books', 'books.id', 'favorites.book_id')
-    .where('favorites.book_id', userId)
-    // .orderBy('books.title', 'ASC')
-    .then((rows) => {
-      const favorites = camelizeKeys(rows);
+    .where({ book_id: bookId, user_id: userId })
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Not Found');
+      }
 
-      res.send(favorites);
+      favorite = camelizeKeys(row);
+      return knex('favorites')
+        .where({ book_id: bookId, user_id: userId })
+        .del()
+    })
+    .then(() => {
+      delete favorite.id;
+      res.send(favorite);
     })
     .catch((err) => {
       next(err);
     });
 });
-// router.get('/favorites:id', authorize, (req, res, next) => {
-//   const { userId } = req.token;
-//
-//   knex('favorites')
-//     // .innerJoin('books', 'books.id', 'favorites.track_id')
-//     .where('favorites.user_id', userId)
-//     // .orderBy('books.title', 'ASC')
-//     .then((rows) => {
-//       const favorites = camelizeKeys(rows);
-//
-//       res.send(favorites);
-//     })
-//     .catch((err) => {
-//       next(err);
-//     });
-// });
 
 module.exports = router;
